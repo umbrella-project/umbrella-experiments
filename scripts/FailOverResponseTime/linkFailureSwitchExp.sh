1baa3838532cd82b153df9cd4957367d2ee4e85b
adibrastegarnia <arastega@purdue.edu>
2018-09-23 15:52:11 -0900

    linkFailureSwitchExp is updated

--- scripts/FailOverResponseTime/linkFailureSwitchExp.sh
+++ scripts/FailOverResponseTime/linkFailureSwitchExp.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 interfaceName1="A2"
 interfaceName2="A4"
-interval=2
+interval=20
 simTime=100
-for j in {2..51..8}
+switchIP=10.10.0.6
+for j in {26..27..8}
 do
-	interval=j
-	ssh nebula106 "iperf3 -s -p 8080" &
-	sleep 3
-	ssh nebula105 "iperf3 -c 10.6.0.6 -t $simTime  -i 1 -p 8080 > client-$interval.log" &
-	numLoops=($simTime/(2 * $interval)) + 1
-	echo $numLoops
-	for i in {1..$numLoops}
+	for k in {1..5}
 	do
-		./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName1 disable","exit";
-		sleep $interval
-		./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName1 enable","exit";
+		interval=$j
+		echo "interval-round:" $interval "," $k
+		 
+		ssh nebula106 "iperf3 -s -p 8080" &
 		sleep 3
-		./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName2 disable","exit";
-		sleep $interval
-		./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName2 enable","exit" ;
-		sleep 3
-		echo $i
-	done
-	sleep 2 
-	ssh nebula106 "pkill iperf3"
-	ssh nebula105 "pkill iperf3"
+		ssh nebula105 "iperf3 -c 10.6.0.6 -t $simTime  -i 1 -p 8080 -P 30 > client-$interval-$k.log" &
+		let "numLoops=(($simTime/(2 * ($interval) + 6)))"
+		#echo "numLoops:" $numLoops 
+		#echo $interval
+		for i in $( eval echo {1..$numLoops} )
+		do
+			./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName1 disable","exit" > /dev/null;
+			sleep $interval
+			./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName1 enable","exit" > /dev/null;
+			sleep 4
+			./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName2 disable","exit" > /dev/null;
+			sleep $interval
+			./procurve-commander.sh nebula 10.10.0.7 "config","interface $interfaceName2 enable","exit" > /dev/null ;
+			sleep 4
+			echo "loops:" "$i"
+		done
+	
+	
+		ssh nebula105 "pkill iperf3"
+		ssh nebula106 "pkill iperf3"
+		sleep 2
+       done
 done
